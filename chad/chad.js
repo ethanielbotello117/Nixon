5efa62fbcab83a108c7fadbbdd411b1c16609e21
Ethaniel Botello <Ebotel200@west-mec.local>
2021-02-05 15:25:13 -0700

    lol

--- chad/chad.js
+++ chad/chad.js
@@ -1,6 +1,12 @@
-let timer = setInterval(time, 500)
+let timer = setInterval(time,  100)
+let poggers = setInterval(pog, 100)
 
 function time() {
     let add = document.createElement("div");
     main.appendChild(add);
+}
+
+function pog(){
+    let lol = document.createElement("section");
+    main.appendChild(lol);
 }
