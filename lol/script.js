8948d1d63c99cea07c5eeb147dba7edda270bef6
Ethaniel Botello <Ebotel200@west-mec.local>
2021-02-08 15:12:21 -0700

    lol

--- lol/script.js
+++ lol/script.js
@@ -1,47 +1,41 @@
 
 
 html.onclick = function(event){
-    let mainCoords = this.getBoundingClientRect();
-    let errorCoords = {
-        top: event.clientY - mainCoords.top - html.clientTop - ewwor.clientHeight/2,
-        left: event.clientX - mainCoords.left - html.clientLeft - ewwor.clientWidth/2,
+        let mainCoords = this.getBoundingClientRect();
+        let errorCoords = {
+            top: event.clientY - mainCoords.top - html.clientTop - ewwor.clientHeight/2,
+            left: event.clientX - mainCoords.left - html.clientLeft - ewwor.clientWidth/2,
+        }
+
+        if(errorCoords.top < 0){
+            errorCoords.top = 0;
+        }
+        if(errorCoords.left < 0){
+            errorCoords.left = 0;
+        }
+
+        if(errorCoords.left + ewwor.clientWidth > html.clientWidth){
+            errorCoords.left = html.clientWidth - ewwor.clientWidth;
+        }
+
+        if(errorCoords.top + ewwor.clientHeight > html.clientHeight){
+            errorCoords.top = html.clientHeight - ewwor.clientHeight;
+        }
+
+        ewwor.style.left = errorCoords.left + 'px';
+        ewwor.style.top = errorCoords.top + 'px';
+
+
+        let timer = setInterval(time, 1)
+        let poggers = setInterval(pog, 1)
+
+    function time() {
+        let add = document.createElement("div");
+        main.appendChild(add);
     }
 
-    if(errorCoords.top < 0){
-        errorCoords.top = 0;
+    function pog(){
+        let lol = document.createElement("section");
+        main.appendChild(lol);
     }
-    if(errorCoords.left < 0){
-        errorCoords.left = 0;
-    }
-
-    if(errorCoords.left + ewwor.clientWidth > html.clientWidth){
-        errorCoords.left = html.clientWidth - ewwor.clientWidth;
-    }
-
-    if(errorCoords.top + ewwor.clientHeight > html.clientHeight){
-        errorCoords.top = html.clientHeight - ewwor.clientHeight;
-    }
-
-    ewwor.style.left = errorCoords.left + 'px';
-    ewwor.style.top = errorCoords.top + 'px';
-
-
-    let timer = setInterval(time, 1)
-    let poggers = setInterval(pog, 1)
-
-function time() {
-    let add = document.createElement("div");
-    main.appendChild(add);
-}
-
-function pog(){
-    let lol = document.createElement("section");
-    main.appendChild(lol);
-}
-
-let oki = function(){
-    alert("P4g3 UnR3$p0s1iv3")
-}
-
-ewwor.addEventListener('contextmenu', oki)
 }
